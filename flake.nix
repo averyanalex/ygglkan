136bbe22f22337a8b4519cf5c2e07b2272ee748a
AveryanAlex <alex@averyan.ru>
2023-05-23 00:10:00 +0300

    Regex support

--- flake.nix
+++ flake.nix
@@ -3,12 +3,11 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
+    import-cargo.url = "github:edolstra/import-cargo";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -16,6 +15,7 @@
     self,
     nixpkgs,
     fenix,
+    import-cargo,
     flake-utils,
     ...
   }:
@@ -23,13 +23,39 @@
       system: let
         overlays = [fenix.overlays.default];
         pkgs = import nixpkgs {inherit system overlays;};
+        inherit (import-cargo.builders) importCargo;
+
         rustVersion = pkgs.fenix.fromToolchainFile {
           file = ./rust-toolchain.toml;
           sha256 = "s9/H9SHOZJdYyJsiqT+cMTspY0SzaaF64ydLiTSfDqQ=";
         };
+
         nativeBuildInputs = [rustVersion pkgs.spirv-tools];
         buildInputs = with pkgs; [vulkan-loader];
+
+        ygglkan = pkgs.stdenv.mkDerivation {
+          name = "ygglkan";
+          src = self;
+
+          inherit buildInputs;
+
+          hardeningDisable = [ "fortify" ];
+
+          nativeBuildInputs = [
+            (importCargo { lockFile = ./Cargo.lock; inherit pkgs; }).cargoHome
+          ] ++ nativeBuildInputs;
+
+          buildPhase = ''
+            cargo build --release --offline
+          '';
+
+          installPhase = ''
+            install -Dm775 ./target/release/ygglkan $out/bin/ygglkan
+          '';
+        };
       in {
+        packages.default = ygglkan;
+
         devShells.default = pkgs.mkShell {
           buildInputs = buildInputs ++ nativeBuildInputs;
           hardeningDisable = [ "fortify" ];
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![feature(slice_flatten)]
 
-use std::sync::atomic::{AtomicU32, Ordering};
+use std::sync::atomic::{AtomicU8, Ordering};
 
 use clap::Parser;
 use hex::ToHex;
@@ -20,6 +20,10 @@ struct Args {
     /// Print hashrate stats
     #[arg(short, long, default_value_t = false)]
     print_stats: bool,
+
+    /// Regex pattern to search
+    #[arg(short, long)]
+    regex: Option<String>,
 }
 
 fn main() {
@@ -41,6 +45,7 @@ fn main() {
         shader_binary,
         args.batch_size * 64,
         args.print_stats,
+        args.regex,
     ));
 }
 
@@ -48,7 +53,13 @@ pub async fn start_internal(
     shader_binary: wgpu::ShaderModuleDescriptorSpirV<'static>,
     batch_size: usize,
     print_stats: bool,
+    regex: Option<String>,
 ) {
+    let re = match regex {
+        Some(r) => Some(regex::Regex::new(&r).unwrap()),
+        None => None,
+    };
+
     let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
         backends: wgpu::Backends::VULKAN,
         ..Default::default()
@@ -134,7 +145,8 @@ pub async fn start_internal(
     let mut new_seeds = vec![[0u8; 32]; batch_size];
     let mut current_seeds = vec![[0u8; 32]; batch_size];
 
-    let max_leading_zeros = AtomicU32::new(16);
+    let max_leading_zeros = AtomicU8::new(0);
+    let mut first_run = true;
 
     rand::thread_rng().fill_bytes(&mut new_seeds.flatten_mut());
     loop {
@@ -159,27 +171,42 @@ pub async fn start_internal(
         queue.write_buffer(&storage_buffer, 0, new_seeds.flatten());
         queue.submit(Some(encoder.finish()));
 
-        pubkeys
-            .par_iter()
-            .zip(current_seeds.par_iter())
-            .for_each(|(pk, seed)| {
-                let mut first_bytes = [0u8; 8];
-                first_bytes.copy_from_slice(&pk[..8]);
-                let first_number = u64::from_be_bytes(first_bytes);
+        if !first_run {
+            pubkeys
+                .par_iter()
+                .zip(current_seeds.par_iter())
+                .for_each(|(pk, seed)| {
+                    let leading_zeros = leading_zeros_of_pubkey(pk);
 
-                let leading_zeros = first_number.leading_zeros();
-                if max_leading_zeros.fetch_max(leading_zeros, Ordering::AcqRel) <= leading_zeros {
-                    let mut sk = [0u8; 64];
-                    sk[..32].copy_from_slice(seed);
-                    sk[32..].copy_from_slice(pk);
-                    let mut lock = std::io::stdout().lock();
-                    writeln!(lock, "=======================================").unwrap();
-                    writeln!(lock, "PrivateKey: {:02X?}", sk.encode_hex::<String>()).unwrap();
-                    writeln!(lock, "PublicKey: {:02X?}", pk.encode_hex::<String>()).unwrap();
-                    writeln!(lock, "Height: {}", leading_zeros).unwrap();
-                    writeln!(lock, "=======================================").unwrap();
-                };
-            });
+                    match &re {
+                        None => {}
+                        Some(re) => {
+                            if max_leading_zeros.load(Ordering::Relaxed) > leading_zeros {
+                                return;
+                            }
+                            if !(re.is_match(&address_for_pubkey(pk).to_string())) {
+                                return;
+                            }
+                        }
+                    }
+
+                    if max_leading_zeros.fetch_max(leading_zeros, Ordering::AcqRel) <= leading_zeros
+                    {
+                        let mut sk = [0u8; 64];
+                        sk[..32].copy_from_slice(seed);
+                        sk[32..].copy_from_slice(pk);
+                        let mut lock = std::io::stdout().lock();
+                        writeln!(lock, "=======================================").unwrap();
+                        writeln!(lock, "PrivateKey: {}", sk.encode_hex::<String>()).unwrap();
+                        writeln!(lock, "PublicKey: {}", pk.encode_hex::<String>()).unwrap();
+                        writeln!(lock, "Address: {}", address_for_pubkey(pk)).unwrap();
+                        writeln!(lock, "Height: {}", leading_zeros).unwrap();
+                        writeln!(lock, "=======================================").unwrap();
+                    };
+                });
+        } else {
+            first_run = false;
+        }
 
         std::mem::swap(&mut current_seeds, &mut new_seeds);
         rand::thread_rng().fill_bytes(&mut new_seeds.flatten_mut());
@@ -197,5 +224,52 @@ pub async fn start_internal(
             let hashrate = batch_size as f64 / time_elapsed / 1_000_000.0;
             println!("Hashrate: {:.2} MH/s", hashrate);
         }
+    }
+}
+
+fn leading_zeros_of_pubkey(pk: &[u8]) -> u8 {
+    let mut zeros = 0u8;
+    for b in pk {
+        let z = b.leading_zeros();
+        zeros += z as u8;
+        if z != 8 {
+            break;
+        }
+    }
+    zeros
+}
+
+fn address_for_pubkey(pk: &[u8]) -> std::net::Ipv6Addr {
+    let zeros = leading_zeros_of_pubkey(pk);
+    let mut buf = [0u8; 16];
+    buf[0] = 0x02;
+    buf[1] = zeros;
+    for (src, trg) in pk[((zeros / 8) as usize)..]
+        .windows(2)
+        .zip(buf[2..].iter_mut())
+    {
+        *trg = src[0].wrapping_shl(((zeros + 1) % 8) as u32)
+            ^ src[1].wrapping_shr(8 - ((zeros + 1) % 8) as u32)
+            ^ 0xFF;
+    }
+    std::net::Ipv6Addr::from(buf)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use crate::address_for_pubkey;
+
+    #[test]
+    fn test_address_for_pubkey() {
+        assert_eq!(
+            address_for_pubkey(
+                hex::decode("000000000c4f58e09d19592f242951e6aa3185bd5ec6b95c0d56c93ae1268cbd")
+                    .unwrap()
+                    .as_slice()
+            ),
+            std::net::Ipv6Addr::from_str("224:7614:e3ec:5cd4:da1b:7ad5:c32a:b9cf").unwrap()
+        )
     }
 }
